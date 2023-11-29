a42317e60ae8cedc8cfabf3fd67f909c2875d949
m3talscarf <151676751+m3talscarf@users.noreply.github.com>
2023-11-29 14:25:58 -0600

    Add files via upload

--- Ubuntun/systemfirewalletc.sh
+++ Ubuntun/systemfirewalletc.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-chmod a+rwx ./systemfirewalletc
+# Gives file perms
+
+chmod a+rwx ./systemfirewalletc.sh
+
+# Setup ufw firewall
 
 sudo apt-get install ufw
 sudo ufw enable
 sudo ufw status
+
+# Updates the system
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+
+# Secures shadow file
+
+sudo chmod 640 /etc/shadow
